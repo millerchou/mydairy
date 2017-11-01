90b792090c0182eee229eaff1fab02acce6db951
trueman <zzrtrue@163.com>
2017-11-01 16:29:27 +0800

    stan

--- Pi/Pi/Pi.h
+++ Pi/Pi/Pi.h
@@ -15,17 +15,4 @@ private:
 	long double w = 0.0;
 };
 
-long double Pi::areas()
-{
-	long double sum = 0;
-	makedw();
-	long double halfw = w / 2;
-	for (int i = 0; i < precision; i++) {
-		double x = i*w + halfw;
-		long double doubleh = r_inch*r_inch - x*x;
-		long double h = sqrt(doubleh);
-		sum += h*w;
-	}
 
-	return sum;
-}
---
+++ Pi/Pi/usr.cpp
@@ -0,0 +1,13 @@
+
+
+#include "stdafx.h"
+
+#include"Pi.h"
+
+int main()
+{
+	Pi a(10000.0);
+	cout << a.areas();
+	getchar();
+	return 0;
+}
